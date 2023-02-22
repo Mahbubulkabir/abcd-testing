6d840dfa7124d2e43d76ab42a3bb9a9ca9a4fe09
Mahbubulkabir <munnagajaghanta@Gmail.com>
2023-02-22 02:11:52 -0500

    MostLike PostComments xml

--- src/test/java/test/TestMostLikePost.java
+++ src/test/java/test/TestMostLikePost.java
@@ -6,36 +6,33 @@ import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.LogInPage;
-import pages.ManageReviews;
+import pages.PostComments;
+import pages.MostLikePost;
 import utility.ReadFromExcel;
 
-public class TestManagerReviews extends CommonAPI {
-
-    Logger LOG = LogManager.getLogger(TestManagerReviews.class.getName());
-    ReadFromExcel read = new ReadFromExcel("C:\\Users\\munna\\InteliiJ\\abcd-GitHub-test-project\\data\\data.xlsx", "test data");
+public class TestMostLikePost extends CommonAPI {
+    Logger LOG = LogManager.getLogger(TestMostLikePost.class.getName());
+    ReadFromExcel read = new ReadFromExcel("C:\\Users\\munna\\InteliiJ\\web-automation-framework-final-Bootcamp\\data\\titles.xlsx", "test data");
     String username= read.getCellValueForGivenHeaderAndKey("key","userName");
     String password= read.getCellValueForGivenHeaderAndKey("key","passWord");
-
     @Test
-    public void managerReviewsFromDropDown() throws InterruptedException {
-        ManageReviews manageReviews = new ManageReviews(getDriver());
+    public void mostLikePostButtonClick() throws InterruptedException {
+        MostLikePost mostLikePost = new MostLikePost(getDriver());
+        PostComments postComments = new PostComments(getDriver());
         LogInPage logInPage = new LogInPage(getDriver());
         String expectedHomePageTitle = "OrangeHRM";
         String actualHomePageTitle = getCurrentTitle();
         Assert.assertEquals(actualHomePageTitle,expectedHomePageTitle);
         LOG.info("land to orangehrm home page successfully");
-
         logInPage.typeUserName(username);
         logInPage.typePassword(password);
         logInPage.setClickOnLogInBtm();
-
-        manageReviews.setClickOnPerformanceBtn();
-        manageReviews.setClickOnDropDownField();
-        manageReviews.setClickOnMyReviews();
+        postComments.setClickOnBuzzBtn();
         Thread.sleep(3000);
-        String expectedUrl= "https://opensource-demo.orangehrmlive.com/web/index.php/performance/myPerformanceReview";
-        Assert.assertEquals(getURL(),expectedUrl);
-        LOG.info("my reviews page success");
+        mostLikePost.setClickOnShareLikeBtn();
+        String expectedUrl="https://opensource-demo.orangehrmlive.com/web/index.php/buzz/viewBuzz";
+        Assert.assertEquals(getURL(), expectedUrl);
+        LOG.info("share post page validation successful");
 
     }
 }
