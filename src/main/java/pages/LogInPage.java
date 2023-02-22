68e71a56c996f0b8f9eeab3b359c45b2d76aeeef
Mahbubulkabir <munnagajaghanta@Gmail.com>
2023-02-22 00:49:20 -0500

    second commit

--- src/main/java/pages/LogInPage.java
+++ src/main/java/pages/LogInPage.java
@@ -1,2 +1,45 @@
-package pages;public class LogInPage {
+package pages;
+
+import base.CommonAPI;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LogInPage extends CommonAPI {
+
+
+    Logger LOG = LogManager.getLogger(LogInPage.class.getName());
+    public LogInPage(WebDriver driver){
+        PageFactory.initElements(driver,this);
+    }
+
+    // object(web element)
+    @FindBy(xpath = "")
+    WebElement userNameField;
+
+    @FindBy(xpath = "")
+    WebElement passwordField;
+
+    @FindBy(xpath = "")
+    WebElement clickOnLogInBtm;
+
+
+    // reusable method
+
+    public void typeUserName(String text){
+        type(userNameField, text);
+    }
+
+    public void typePassword(String text){
+        type(passwordField, text);
+    }
+    public void setClickOnLogInBtm(){
+        clickOn(clickOnLogInBtm);
+        LOG.info("logIn page validation success");
+    }
+
 }
+
